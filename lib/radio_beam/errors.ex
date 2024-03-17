bbbe2144f48ab65a83ff02f873a88b02b21d7724
Bentheburrito <lolitsaburrito@gmail.com>
2024-03-16 19:47:11 -0700

    error fxns + tests

---
+++ lib/radio_beam/errors.ex
@@ -0,0 +1,51 @@
+defmodule RadioBeam.Errors do
+  @moduledoc """
+  Error helpers
+  """
+
+  @typedoc "A standard error response."
+  @type t() :: %{required(:errcode) => String.t(), required(:error) => String.t()}
+
+  def std_errors do
+    ~w(forbidden unknown_token missing_token bad_json not_json not_found limit_exceeded unrecognized unknown)a
+  end
+
+  def forbidden(message \\ "You do not have sufficient access to this resource"),
+    do: std_error_res("M_FORBIDDEN", message)
+
+  def unknown_token(message \\ "The access/refresh token is not known. Please re-authenticate", soft_logout? \\ false),
+    do: "M_UNKNOWN_TOKEN" |> std_error_res(message) |> Map.put(:soft_logout, soft_logout?)
+
+  def missing_token(message \\ "Please provide an access token"), do: std_error_res("M_MISSING_TOKEN", message)
+
+  def bad_json(message \\ "Params were missing from your request"), do: std_error_res("M_BAD_JSON", message)
+
+  def not_json(message \\ "Bad JSON payload"), do: std_error_res("M_NOT_JSON", message)
+
+  def not_found(message \\ "Resource not found"), do: std_error_res("M_NOT_FOUND", message)
+
+  def limit_exceeded(message \\ "You have been making too many requests. Please wait before trying again"),
+    do: std_error_res("M_LIMIT_EXCEEDED", message)
+
+  def unrecognized(message \\ "This homeserver does not implement that endpoint for the given HTTP method"),
+    do: std_error_res("M_UNRECOGNIZED", message)
+
+  def unknown(message \\ "An unknown error has occurred"), do: std_error_res("M_UNKNOWN", message)
+
+  def endpoint_error(errcode, error) when is_atom(errcode) do
+    errcode = errcode |> to_string() |> String.upcase()
+    endpoint_error("M_" <> errcode, error)
+  end
+
+  def endpoint_error(errcode, error) when is_binary(errcode) and is_binary(error) do
+    std_error_res(errcode, error)
+  end
+
+  @spec std_error_res(String.t(), String.t()) :: t()
+  defp std_error_res(errcode, error) do
+    %{
+      errcode: errcode,
+      error: error
+    }
+  end
+end
---
+++ test/radio_beam/errors_test.exs
@@ -0,0 +1,27 @@
+defmodule RadioBeam.ErrorsTest do
+  use ExUnit.Case
+
+  alias RadioBeam.Errors
+
+  describe "standard errors" do
+    test "MUST have `error` and `errcode` keys" do
+      for std_err <- Errors.std_errors() do
+        assert %{errcode: _, error: _} = apply(Errors, std_err, [])
+      end
+    end
+  end
+  
+  describe "endpoint errors" do
+    test "can handle atom errcodes" do
+      assert %{errcode: "M_UNAUTHORIZED"} = Errors.endpoint_error(:unauthorized, "")
+    end
+
+    test "can handle string errcodes" do
+      assert %{errcode: "M_UNAUTHORIZED"} = Errors.endpoint_error("M_UNAUTHORIZED", "")
+    end
+
+    test "returns an error message" do
+      assert %{error: "You do not have access"} = Errors.endpoint_error("M_UNAUTHORIZED", "You do not have access")
+    end
+  end
+end
