2b411a06012c85e22056a52deb22c76e7836e4b6
Bentheburrito <lolitsaburrito@gmail.com>
2024-06-15 15:13:34 -0700

    move create room schema to own module

--- lib/radio_beam_web/controllers/room_controller.ex
+++ lib/radio_beam_web/controllers/room_controller.ex
@@ -3,11 +3,11 @@ defmodule RadioBeamWeb.RoomController do
 
   require Logger
 
-  alias Polyjuice.Util.Schema
   alias RadioBeam.{Errors, Room, User}
+  alias RadioBeamWeb.Schemas.Room, as: RoomSchema
 
   plug RadioBeamWeb.Plugs.Authenticate
-  plug RadioBeamWeb.Plugs.EnforceSchema, get_schema: {__MODULE__, :schema, []}
+  plug RadioBeamWeb.Plugs.EnforceSchema, [get_schema: {RoomSchema, :create, []}] when action == :create
 
   def create(conn, _params) do
     %User{} = creator = conn.assigns.user
@@ -57,54 +57,4 @@ defmodule RadioBeamWeb.RoomController do
         |> json(Errors.endpoint_error(:invalid_room_state, "The provided initial state is invalid."))
     end
   end
-
-  def schema() do
-    %{available: available_room_versions, default: default_room_version} =
-      Application.get_env(:radio_beam, :capabilities)[:"m.room_versions"]
-
-    %{
-      "creation_content" => optional(content_schema()),
-      "initial_state" => optional(:any),
-      "invite" => optional(:any),
-      "invite_3pid" => optional(:any),
-      "is_direct" => optional(:boolean),
-      "name" => optional(:string),
-      "power_level_content_override" => [:any, default: %{}],
-      "preset" =>
-        optional(
-          Schema.enum(%{
-            "private_chat" => :private_chat,
-            "public_chat" => :public_chat,
-            "trusted_private_chat" => :trusted_private_chat
-          })
-        ),
-      "room_alias_name" => optional(&room_localpart/1),
-      "room_version" => [Schema.enum(Map.keys(available_room_versions)), default: default_room_version],
-      "topic" => optional(:string),
-      "visibility" => [Schema.enum(%{"private" => :private, "public" => :public}), default: :private]
-    }
-  end
-
-  defp content_schema() do
-    %{
-      "m.federate" => [:boolean, default: true],
-      "predecessor" => optional(&Schema.room_id/1),
-      "type" => optional(:string),
-      # these will just be overwritten
-      "room_version" => optional(:any),
-      "creator" => optional(:any)
-    }
-
-    # case Integer.parse(room_version) do
-    #   {version_num, ""} when version_num in 1..10 -> Map.put(schema, "creator", &Schema.user_id/1)
-    #   _ -> schema
-    # end
-  end
-
-  # TODO: validate localpart grammar
-  defp room_localpart(localpart) do
-    Schema.room_alias("##{localpart}:#{RadioBeam.server_name()}")
-  end
-
-  defp optional(type), do: [type, :optional]
 end
---
+++ lib/radio_beam_web/schemas/room.ex
@@ -0,0 +1,53 @@
+defmodule RadioBeamWeb.Schemas.Room do
+  alias Polyjuice.Util.Schema
+
+  def create do
+    %{available: available_room_versions, default: default_room_version} =
+      Application.get_env(:radio_beam, :capabilities)[:"m.room_versions"]
+
+    %{
+      "creation_content" => optional(content_schema()),
+      "initial_state" => optional(:any),
+      "invite" => optional(:any),
+      "invite_3pid" => optional(:any),
+      "is_direct" => optional(:boolean),
+      "name" => optional(:string),
+      "power_level_content_override" => [:any, default: %{}],
+      "preset" =>
+        optional(
+          Schema.enum(%{
+            "private_chat" => :private_chat,
+            "public_chat" => :public_chat,
+            "trusted_private_chat" => :trusted_private_chat
+          })
+        ),
+      "room_alias_name" => optional(&room_localpart/1),
+      "room_version" => [Schema.enum(Map.keys(available_room_versions)), default: default_room_version],
+      "topic" => optional(:string),
+      "visibility" => [Schema.enum(%{"private" => :private, "public" => :public}), default: :private]
+    }
+  end
+
+  defp content_schema() do
+    %{
+      "m.federate" => [:boolean, default: true],
+      "predecessor" => optional(&Schema.room_id/1),
+      "type" => optional(:string),
+      # these will just be overwritten
+      "room_version" => optional(:any),
+      "creator" => optional(:any)
+    }
+
+    # case Integer.parse(room_version) do
+    #   {version_num, ""} when version_num in 1..10 -> Map.put(schema, "creator", &Schema.user_id/1)
+    #   _ -> schema
+    # end
+  end
+
+  # TODO: validate localpart grammar
+  defp room_localpart(localpart) do
+    Schema.room_alias("##{localpart}:#{RadioBeam.server_name()}")
+  end
+
+  defp optional(type), do: [type, :optional]
+end
